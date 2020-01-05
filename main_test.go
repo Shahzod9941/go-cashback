eb6f9a621b28dfb145e381890a8c4c5b3006b3ba
Shahzod9941 <shahzodtoatov@gmail.com>
2020-01-05 10:28:16 +0300

    initial version

--- main_test.go
+++ main_test.go
@@ -10,14 +10,14 @@ func Test_cashback(t *testing.T) {
 		want int
 	}{
 		// TODO: Add test cases.
-		{"Have cashback", amount:5000, want:250}
-		{"No cashback", amount:1000, want:0}
-		{"Cashback on bound", amount:3000, want:150}
+		{name:"Have cashback", amount:5000, want:250}
+		{name:"No cashback", amount:1000, want:0}
+		{name:"Cashback on bound", amount:3000, want:150}
 	}
 	for _, test := range tests {
 		got := cashback(test.amount)
-		if got != test.want{
-			t.error("forcashbaxk test", test.name, "got:", got, "want:", test.want)
+		if got != test.want {
+			t.error("for cashbaxk test", test.name, "got:", got, "want:", test.want)
 	}
 	}
 
